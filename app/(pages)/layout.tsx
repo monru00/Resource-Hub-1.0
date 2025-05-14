0421835912e72b53b48b17158f88a8f081a6ba13
monru00 <mohammednesru00@gmail.com>
2025-05-14 23:04:09 +0300

    initial commit

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,234 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PagesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      {/* HEADER  */}
+      <header className="flex flex-wrap lg:justify-start lg:flex-nowrap z-50 w-full">
+        <nav className="w-full flex flex-wrap lg:grid lg:grid-cols-12 basis-full items-center px-4 md:px-6 lg:px-8 mx-auto fixed z-50 bg-white/30 backdrop-blur-md border-b border-white/20 shadow-sm">
+          <div className="lg:col-span-3 flex items-center">
+            {/* Logo */}
+            <Link href="/home" className="pr-3">
+              <Image src="/images/logo.png" alt="" width={60} height={60} />
+            </Link>
+            <p className="text-xl">Resource Hub</p>
+            {/* End Logo */}
+
+            <div className="ms-1 sm:ms-2"></div>
+          </div>
+
+          {/* Button Group */}
+          <div className="flex items-center gap-x-1 lg:gap-x-2 ms-auto py-1 lg:ps-6 lg:order-3 lg:col-span-3">
+            <button
+              type="button"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium text-nowrap rounded-xl bg-white border border-gray-200 text-black hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white dark:focus:text-white"
+            >
+              Sign in
+            </button>
+            <button
+              type="button"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium text-nowrap rounded-xl border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 transition disabled:opacity-50 disabled:pointer-events-none"
+            >
+              Bootmarks
+            </button>
+
+            <div className="lg:hidden">
+              <button
+                type="button"
+                className="hs-collapse-toggle size-9.5 flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                id="hs-navbar-hcail-collapse"
+                aria-expanded="false"
+                aria-controls="hs-navbar-hcail"
+                aria-label="Toggle navigation"
+                data-hs-collapse="#hs-navbar-hcail"
+              >
+                <svg
+                  className="hs-collapse-open:hidden shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+                <svg
+                  className="hs-collapse-open:block hidden shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          {/* End Button Group */}
+
+          {/* Collapse */}
+          <div
+            id="hs-navbar-hcail"
+            className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow lg:block lg:w-auto lg:basis-auto lg:order-2 lg:col-span-6"
+            aria-labelledby="hs-navbar-hcail-collapse"
+          >
+            <div className="flex flex-col gap-y-4 gap-x-0 mt-5 lg:flex-row lg:justify-center lg:items-center lg:gap-y-0 lg:gap-x-7 lg:mt-0">
+              <div>
+                <a
+                  className="relative inline-block text-black focus:outline-hidden before:absolute before:bottom-0.5 before:start-0 before:-z-1 before:w-full before:h-1 before:bg-blue-400 dark:text-white"
+                  href="#"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </div>
+              <div>
+                <a
+                  className="inline-block text-black hover:text-gray-600 focus:outline-hidden focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300"
+                  href="#"
+                >
+                  Resources
+                </a>
+              </div>
+              <div>
+                <a
+                  className="inline-block text-black hover:text-gray-600 focus:outline-hidden focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300"
+                  href="#"
+                >
+                  FAQ
+                </a>
+              </div>
+              <div>
+                <a
+                  className="inline-block text-black hover:text-gray-600 focus:outline-hidden focus:text-gray-600 dark:text-white dark:hover:text-neutral-300 dark:focus:text-neutral-300"
+                  href="#"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* End Collapse */}
+        </nav>
+      </header>
+      {/* END HEADER */}
+      <main className="flex-1">{children}</main>
+      // footer
+      <footer className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+        {/* Grid */}
+        <div className="text-center">
+          <div>
+            <a
+              className="flex-none text-xl font-semibold text-black dark:text-white"
+              href="#"
+              aria-label="Brand"
+            >
+              Brand
+            </a>
+          </div>
+          {/* End Col */}
+
+          <div className="mt-3">
+            <p className="text-gray-500 dark:text-neutral-500">
+              We're part of the{" "}
+              <a
+                className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
+                href="#"
+              >
+                Htmlstream
+              </a>{" "}
+              family.
+            </p>
+            <p className="text-gray-500 dark:text-neutral-500">
+              © 2025 Preline Labs.
+            </p>
+          </div>
+
+          {/* Social Brands */}
+          <div className="mt-3 space-x-2">
+            <a
+              className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              href="#"
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+              </svg>
+            </a>
+            <a
+              className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              href="#"
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+              </svg>
+            </a>
+            <a
+              className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              href="#"
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </a>
+            <a
+              className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              href="#"
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
+              </svg>
+            </a>
+          </div>
+          {/* End Social Brands */}
+        </div>
+        {/* End Grid */}
+      </footer>
+      {/* END FOOTER */}
+    </div>
+  );
+}
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,94 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    // Hero
+    <div className="relative overflow-hidden">
+      {/* Gradients */}
+      <div
+        aria-hidden="true"
+        className="flex absolute -top-96 start-1/2 transform -translate-x-1/2"
+      >
+        <div className="bg-linear-to-r from-violet-300/50 to-purple-100 blur-3xl w-100 h-175 rotate-[-60deg] transform -translate-x-40 dark:from-violet-900/50 dark:to-purple-900"></div>
+        <div className="bg-linear-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-[1440px] h-200 rounded-fulls origin-top-left -rotate-12 -translate-x-60 dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70"></div>
+      </div>
+      {/* End Gradients */}
+
+      <div className="relative z-10">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
+          <div className="max-w-2xl text-center mx-auto">
+            {/* Title */}
+            <div className="mt-5 max-w-2xl">
+              <h1 className="block font-semibold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
+                Get What you want need
+              </h1>
+            </div>
+            {/* End Title */}
+
+            <div className="mt-5 max-w-3xl">
+              <p className="text-lg text-gray-600 dark:text-neutral-400">
+                Preline UI is an open-source set of prebuilt UI components,
+                ready-to-use examples and Figma design system based on the
+                utility-first Tailwind CSS framework.
+              </p>
+            </div>
+
+            {/* Buttons */}
+            <div className="mt-8 gap-3 flex justify-center">
+              <a
+                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                href="#"
+              >
+                Get started
+                <Image
+                  src="/images/greaterthan.svg"
+                  alt=""
+                  width={12}
+                  height={12}
+                />
+              </a>
+              <a
+                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                href="#"
+              >
+                <Image
+                  src="/images/feedback.svg"
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+                Feed back
+              </a>
+            </div>
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            <h1>hi</h1>
+            <br />
+            {/* End Buttons */}
+          </div>
+        </div>
+      </div>
+    </div>
+    // End Hero
+  );
+}
