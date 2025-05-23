72b953a69c6046bb893faf5ac6b180e65b676787
monru00 <mohammednesru00@gmail.com>
2025-05-23 16:30:53 +0300

    resources page layout change

--- app/(pages)/resources/page.tsx
+++ app/(pages)/resources/page.tsx
@@ -1,216 +1,206 @@
+import Image from "next/image";
+
 export default function ResourcesPage() {
   return (
     <div className="py-30">
-      {/*Icon Blocks */}
+      {/* Icon Blocks */}
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-center gap-6 md:gap-10">
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <circle cx="13.5" cy="6.5" r=".5" />
-                  <circle cx="17.5" cy="10.5" r=".5" />
-                  <circle cx="8.5" cy="7.5" r=".5" />
-                  <circle cx="6.5" cy="12.5" r=".5" />
-                  <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z" />
-                </svg>
+                <Image src="/images/ui.svg" width={30} height={30} alt="" />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Build your portfolio
+                  Frontend & UI/UX
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              The simplest way to keep your portfolio always up-to-date.
+              Build beautiful UI and learn UI/UX faster with the right tools and
+              inspirations.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
 
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M2 3h20" />
-                  <path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3" />
-                  <path d="m7 21 5-5 5 5" />
-                </svg>
+                <Image
+                  src="/images/backend.svg"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Get freelance work
+                  Backend & API
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              New design projects delivered to your inbox each morning.
+              Power your apps with robust backends, APIs, and server-side
+              solutions.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
 
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7" />
-                  <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
-                  <path d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4" />
-                  <path d="M2 7h20" />
-                  <path d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12v0a2 2 0 0 1-2-2V7" />
-                </svg>
+                <Image
+                  src="/images/database.svg"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Sell your goods
+                  Database
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              Get your goods in front of millions of potential customers with
-              ease.
+              Store, manage, and query data with ease using the best database
+              resources.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
 
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M5.5 8.5 9 12l-3.5 3.5L2 12l3.5-3.5Z" />
-                  <path d="m12 2 3.5 3.5L12 9 8.5 5.5 12 2Z" />
-                  <path d="M18.5 8.5 22 12l-3.5 3.5L15 12l3.5-3.5Z" />
-                  <path d="m12 15 3.5 3.5L12 22l-3.5-3.5L12 15Z" />
-                </svg>
+                <Image
+                  src="/images/algorithm.svg"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Get freelance work
+                  Technical Skills
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              New design projects delivered to your inbox each morning.
+              Sharpen your toolkit with practical skills, tips, and deep dives
+              to Dsa and job preparation.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
 
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M16.466 7.5C15.643 4.237 13.952 2 12 2 9.239 2 7 6.477 7 12s2.239 10 5 10c.342 0 .677-.069 1-.2" />
-                  <path d="m15.194 13.707 3.814 1.86-1.86 3.814" />
-                  <path d="M19 15.57c-1.804.885-4.274 1.43-7 1.43-5.523 0-10-2.239-10-5s4.477-5 10-5c4.838 0 8.873 1.718 9.8 4" />
-                </svg>
+                <Image
+                  src="/images/programming.svg"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Sell your goods
+                  Learn Programming
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              Get your goods in front of millions of potential customers with
-              ease.
+              Start coding from scratch or level up with curated learning paths
+              and resources.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
 
-          {/*Card */}
-          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900">
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
             <div className="flex items-center gap-x-4 mb-3">
               <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-                <svg
-                  className="shrink-0 size-6 text-blue-600 dark:text-blue-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M8.3 10a.7.7 0 0 1-.626-1.079L11.4 3a.7.7 0 0 1 1.198-.043L16.3 8.9a.7.7 0 0 1-.572 1.1Z" />
-                  <rect x="3" y="14" width="7" height="7" rx="1" />
-                  <circle cx="17.5" cy="17.5" r="3.5" />
-                </svg>
+                <Image src="/images/repo.svg" width={30} height={30} alt="" />
               </div>
               <div className="shrink-0">
                 <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-                  Build your portfolio
+                  Educational Repos & Analytics
                 </h3>
               </div>
             </div>
             <p className="text-gray-600 dark:text-neutral-400">
-              The simplest way to keep your portfolio always up-to-date.
+              Explore real projects and insights to learn how developers build
+              at scale.
             </p>
           </div>
-          {/*End Card */}
+          {/* End Card */}
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
+            <div className="flex items-center gap-x-4 mb-3">
+              <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
+                <Image src="/images/tools.svg" width={30} height={30} alt="" />
+              </div>
+              <div className="shrink-0">
+                <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
+                  Development Tools
+                </h3>
+              </div>
+            </div>
+            <p className="text-gray-600 dark:text-neutral-400">
+              Streamline your workflow with must-have tools, extensions, and
+              utilities.
+            </p>
+          </div>
+          {/* End Card */}
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
+            <div className="flex items-center gap-x-4 mb-3">
+              <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
+                <Image src="/images/ai.svg" width={30} height={30} alt="" />
+              </div>
+              <div className="shrink-0">
+                <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
+                  Ai & Automation
+                </h3>
+              </div>
+            </div>
+            <p className="text-gray-600 dark:text-neutral-400">
+              Boost productivity with AI tools, scripts, and smart automation
+              hacks.
+            </p>
+          </div>
+          {/* End Card */}
+          {/* Card */}
+          <div className="size-full bg-white shadow-lg rounded-lg p-5 dark:bg-neutral-900 transition-all duration-300 hover:bg-gray-100 hover:shadow-xl hover:scale-105 dark:hover:bg-neutral-800">
+            <div className="flex items-center gap-x-4 mb-3">
+              <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
+                <Image
+                  src="/images/resources.svg"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
+              </div>
+              <div className="shrink-0">
+                <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
+                  Developer Resources
+                </h3>
+              </div>
+            </div>
+            <p className="text-gray-600 dark:text-neutral-400">
+              From docs to communities — everything you need to grow as a
+              developer.
+            </p>
+          </div>
+          {/* End Card */}
         </div>
       </div>
-      {/*End Icon Blocks */}
+      {/* End Icon Blocks */}
     </div>
   );
 }
